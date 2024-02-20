590ce97f0b39e65ee7cccf4d53419683b3f94e61
Cuong <72295193+Cuong15061999@users.noreply.github.com>
2024-02-20 17:46:03 +0700

    refactor: synchronize naming with user api

--- app/api/conversations.py
+++ app/api/conversations.py
@@ -6,31 +6,31 @@ from app.models.conversations import ConversationModel, ResponseConversationMode
 conversations_router = APIRouter(prefix="/api/v1/conversation", tags=["Conversation"])
 
 @conversations_router.get("/{id}")
-async def get_conversation(id: str, repos_manager: RepositoryManager = Depends()) -> dict:
-    conversation = await repos_manager.conversation.get_by_id(id)
+async def get_conversation(id: str, repositories: RepositoryManager = Depends()) -> dict:
+    conversation = await repositories.conversation.get_by_id(id)
     return conversation
 
 @conversations_router.get("/all-info/{id}")
-async def get_conversation_all_info(id: str, repos_manager: RepositoryManager = Depends()) -> dict:
-    conversation = await repos_manager.conversation.get_by_id_all_info(id)
+async def get_conversation_all_info(id: str, repositories: RepositoryManager = Depends()) -> dict:
+    conversation = await repositories.conversation.get_by_id_all_info(id)
     return conversation
 
 @conversations_router.get("/")
-async def get_conversations(repos_manager: RepositoryManager = Depends()) -> list[ResponseConversationModel]:
-    conversations = await repos_manager.conversation.get_all()
+async def get_conversations(repositories: RepositoryManager = Depends()) -> list[ResponseConversationModel]:
+    conversations = await repositories.conversation.get_all()
     return conversations
 
 @conversations_router.post("/")
-async def create_conversation(conversation: ConversationModel, repos_manager: RepositoryManager = Depends()):
-    await repos_manager.conversation.create(conversation)
+async def create_conversation(conversation: ConversationModel, repositories: RepositoryManager = Depends()):
+    await repositories.conversation.create(conversation)
     return HTTPStatus.NO_CONTENT
 
 @conversations_router.put("/{id}")
-async def update_conversation(id: str, conversation: ConversationModel, repos_manager: RepositoryManager =Depends()):
-    await repos_manager.conversation.update(id, conversation)
+async def update_conversation(id: str, conversation: ConversationModel, repositories: RepositoryManager =Depends()):
+    await repositories.conversation.update(id, conversation)
     return HTTPStatus.NO_CONTENT
 
 @conversations_router.delete("/{id}")
-async def delete_conversation(id: str, repos_manager: RepositoryManager = Depends()):
-    await repos_manager.conversation.remove(id)
+async def delete_conversation(id: str, repositories: RepositoryManager = Depends()):
+    await repositories.conversation.remove(id)
     return HTTPStatus.NO_CONTENT
