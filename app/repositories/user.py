5c1dcfbb1fe5f73e2db6cafdb3d1cb64d0096c54
Cuong <72295193+Cuong15061999@users.noreply.github.com>
2024-02-20 21:54:44 +0700

    feat: user service

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -1,20 +1,27 @@
+from ast import Dict
+from typing import Optional
 from app.utils.serialize import serialize_dict, serialize_list
 from .base import BaseRepository
 from bson import ObjectId
-from app.models.users import UserModel
+from app.models.users import ResponseUserModel, UserModel
 
 class UserRepository(BaseRepository):
     def __init__(self, users_collection) -> None:
         self.users_collection = users_collection
 
-    async def get_by_id(self, id: str) -> dict:
+    async def get_by_id(self, id: str) -> dict[ResponseUserModel]:
+        #right now serialize function is return dict and None type. 
+        #how to make it return dynamic ResponseUserModel or ResponseConversationModel ...
         return serialize_dict(await self.users_collection.find_one({"_id": ObjectId(id)}))
+     
+    async def get_by_email(self, email: str) -> dict:
+        return serialize_dict(await self.users_collection.find_one({"email": email}))
 
     async def get_all(self):
         return await serialize_list(self.users_collection.find())
 
     async def create(self, user: UserModel):
-        await self.users_collection.insert_one(user)
+        await self.users_collection.insert_one(dict(user))
 
     async def update(self, id: str,  user: UserModel):
         await self.users_collection.update_one({"_id": ObjectId(id)}, {"$set": dict(user)})
--- app/services/user.py
+++ app/services/user.py
@@ -1,12 +1,47 @@
-
-
 from fastapi import Depends
+from app.models.users import UserModel
 from app.repositories.manger import RepositoryManager
 
 
 class UserService():
-    def __init__(self, repos_manager: RepositoryManager = Depends()) -> None:
-        self.repos_manager = repos_manager
+    def __init__(self, repositories: RepositoryManager = Depends()) -> None:
+        self.repositories = repositories
+
+    async def get_by_id(self, id: str):
+        user = await self.repositories.user.get_by_id(id)
+        if user is None:
+            return {"message": "Not Found User"}
+        else:
+            return user
 
     async def get_all(self):
-        return await self.repos_manager.user.get_all()
+        return await self.repositories.user.get_all()
+    
+    async def create(self, user: UserModel):
+        existed_user_by_email = await self.repositories.user.get_by_email(user.email)
+        if existed_user_by_email is None:
+            await self.repositories.user.create(user)
+            new_user = await self.repositories.user.get_by_email(user.email)
+            return new_user
+        else:
+            return {"message": f"User with email '{user.email}' already exists"}
+
+    async def update(self, id: str,  user: UserModel):
+        existed_user_by_id = await self.repositories.user.get_by_id(id)
+        existed_user_by_email = await self.repositories.user.get_by_email(user.email)
+        print(existed_user_by_id.get("_id"))
+        if existed_user_by_id is None:
+            return {"message": f"User with id '{id}' is not exist"}
+        if existed_user_by_id and existed_user_by_email and existed_user_by_id.get("_id") != existed_user_by_email.get("_id"):
+            return {"message": f"User with email already used"}
+        else:
+            await self.repositories.user.update(id, user)
+            return {"message": f"Update success"}
+
+    async def remove(self, id: str):
+        existed_user_by_id = await self.repositories.user.get_by_id(id)
+        if existed_user_by_id is None:
+            return {"message": f"User with id '{id}' is not exist"}
+        else:
+            await self.repositories.user.remove(id)
+            return {"message": f"User with id '{id}' deleted"}
