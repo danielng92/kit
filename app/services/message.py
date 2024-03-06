af276c13d408c44abe586f396d435e4fb3e069cc
Daniel <hiendat9299@gmail.com>
2024-03-06 16:10:50 +0200

    setting up message services.

--- app/services/message.py
+++ app/services/message.py
@@ -1,3 +1,4 @@
+from app.common.exceptions.not_found import NotFoundException
 from app.models.messages import MessageModel
 from app.repositories.manger import RepositoryManager
 
@@ -9,13 +10,26 @@ class MessageService():
     async def get_all(self) -> None:
         raise Exception ("This is not implemented")
 
-    async def update(self, id: str, message: MessageModel) -> None:
+    async def update(self, message: MessageModel) -> None:
+        user =await self.repositories.user.get_by_id(message.sender_id)
+        if user is None:
+            raise NotFoundException(f"Not found user with this id: {message.sender_id}")
         await self.repositories.message.update(message)
 
     async def create(self, message: MessageModel) -> None:
+        user =await self.repositories.user.get_by_id(message.sender_id)
+        if user is None:
+            raise NotFoundException(f"Not found user with this id: {message.sender_id}")
         await self.repositories.message.create(message)    
 
     async def remove(self, message: MessageModel) -> None:
         await self.repositories.message.remove(message)
     
-    
+    async def get_by_conversation_id(self, conversation_id: str) -> list[MessageModel]:
+        conversation= await self.repositories.conversation.get_by_id(conversation_id)
+        if conversation is None:
+            raise NotFoundException(f"Not found conversation with this id: {conversation_id}")
+        
+        return await self.repositories.message.get_by_conversation_id(conversation_id)
+        
+        
