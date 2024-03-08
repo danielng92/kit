1e858aa597719dbb84fe5dd4e3594ed1fcc48eeb
LAPTOP-0RB3OV43\ASUS <pmhien2703@gmail.com>
2024-03-08 20:35:03 +0700

    feat: add userservice into service manager

--- app/services/manger.py
+++ app/services/manger.py
@@ -1,4 +1,5 @@
 from app.services.conversation import ConversationService
+from app.services.message import MessageService
 from app.services.user import UserService
 from app.repositories.manger import RepositoryManager
 from typing import Annotated
@@ -7,11 +8,13 @@ class ServiceManager:
     _repo: RepositoryManager
     _user: UserService
     _conversation: ConversationService
+    _message: MessageService
 
     def __init__(self, repo: RepositoryManager = Depends()) -> None:
         self._repo = repo
         self._user = None
         self._conversation = None
+        self._message = None
 
     @property
     def user(self) -> UserService:
@@ -24,4 +27,10 @@ class ServiceManager:
         if(self._conversation is None):
             self._conversation = ConversationService(self._repo)
         return self._conversation
+    
+    @property
+    def message(self) -> MessageService:
+        if(self._message is None):
+            self._message = MessageService(self._repo)
+        return self._message
     
